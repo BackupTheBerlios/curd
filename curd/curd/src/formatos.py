90da064caa42aa2bceb9823fe16c4f10bc31177d
xsl4v3 <xsl4v3>
2007-06-23 00:22:27 +0000

    Initial revision

---
+++ curd/curd/src/formatos.py
@@ -0,0 +1,12 @@
+formatos = [ 
+'Formatos' 		 , 'legenda'   , 
+'Nokia s60/NGAGE [3gp]'  , '3gp'       ,
+'DV [PAL]' 		 , 'pal-dv'    , 
+'DV [NTSC]' 		 , 'ntsc-dv'   ,
+'DVD [PAL]' 		 , 'pal-dvd'   , 
+'DVD [NTSC]' 		 , 'ntsc-dvd'  ,
+'VCD [PAL]' 		 , 'pal-vcd'   ,  
+'VCD [NTSC]' 		 , 'ntsc-vcd'  ,
+'SVCD [PAL]' 		 , 'pal-svcd'  , 
+'SVCD [NTSC]' 		 , 'ntsc-svcd'  
+]
